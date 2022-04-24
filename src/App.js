d01b42904fdf40311bdd1bc84023099acf5adbb4
tzfocus7 <emmanuelexfrancis@gmail.com>
2022-04-24 17:23:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar.Js';
+import Home from './Pages/Home/Home.Js';
+import About from './Pages/About/About.Js';
+import Product from './Pages/Product/Product';
+import Contact from './Pages/Contact/Contact.Js';
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/About' element={<About/>}/>
+          <Route path='/Product' element={<Product/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
