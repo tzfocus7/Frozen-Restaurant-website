9d4736ff1505d9f352300d92c0f883f2271347a4
tzfocus7 <emmanuelexfrancis@gmail.com>
2022-04-24 21:57:54 +0100

    added route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-
+import React from 'react'
 import './App.css';
-import Navbar from './Components/Navbar/Navbar.Js';
-import Home from './Pages/Home/Home.Js';
-import About from './Pages/About/About.Js';
+import Navbar from './Components/Navbar/Navbar.js';
+import Home from './Pages/Home/Home.js';
+import About from './Pages/About/About.js';
 import Product from './Pages/Product/Product';
-import Contact from './Pages/Contact/Contact.Js';
+import Contact from './Pages/Contact/Contact.js';
 import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar/>
+     
+
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/About' element={<About/>}/>
           <Route path='/Product' element={<Product/>}/>
           <Route path='/Contact' element={<Contact/>}/>
+
         </Routes>
 
       </BrowserRouter>
     </div>
   );
+
 }
 
 export default App;
